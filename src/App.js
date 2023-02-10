4edae32f2e717a570cc636a31b92d2e40a8af1c7
Leon Forsythe <lvforsythe95@gmail.com>
2023-02-10 18:34:12 -0500

    Cleaned up default react files. Created a Menu page and reusable card component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Menu from './pages/Menu';
+
+function App() {
+  return (
+    <Menu/>
+  );
+}
+
+export default App;
---
+++ src/pages/Menu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../components/Card";
+
+export default function Menu(){
+    return (
+        <Card>
+            <h1>Sushi</h1>
+            <p>$8.99</p>
+            <p>Description description description</p>
+            <button>Add to cart</button>
+        </Card>
+    )
+}
